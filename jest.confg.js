0d09d7df6f2010b15d3e7647c3614d23a334376f
Saheedatt <afolabisaheedat1@gmail.com>
2024-07-28 06:23:41 +0100

    Unit testing failing

---
+++ jest.confg.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+  };
+  
+  
